ae808beb5ccffc0a1dd64a3462f9ef7e06d0db7a
SachinYadavAug20 <samtagon777@gmail.com>
2026-08-01 18:07:08 +0530

    Added routs for sign in and up

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'expo-router'
+import { View, Text } from 'react-native'
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+      <Link href="/(auth)/sign-up" >Create Account</Link>
+    </View>
+  )
+}
+
+export default SignIn
--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function App() {
@@ -6,6 +7,9 @@ export default function App() {
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to onboarding</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Sign In</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Sign Up</Link>
     </View>
   );
 }
