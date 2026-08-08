2c575643c75431be3981a192fef8b9f8692e52fb
SachinYadavAug20 <samtagon777@gmail.com>
2026-08-08 16:08:19 +0530

    Implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,47 +1,29 @@
 import { Link } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
-import {styled} from 'nativewind';
-const SafeAreaView =styled(RNSafeAreaView);
+import { styled } from "nativewind";
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
+      <Text className="text-xl font-bold text-primary text-7xl font-sans-extrabold">
+        Home
       </Text>
       <Link href="/onboarding" asChild>
-        <Pressable className="mt-4 rounded bg-primary px-6 py-3">
+        <Pressable className="mt-4 font-sans-bold rounded bg-primary px-6 py-3">
           <Text className="text-center text-base text-white">
             Go to onboarding
           </Text>
         </Pressable>
       </Link>
       <Link href="/(auth)/sign-in" asChild>
-        <Pressable className="mt-4 rounded bg-primary px-6 py-3">
+        <Pressable className="mt-4 font-sans-bold rounded bg-primary px-6 py-3">
           <Text className="text-center text-base text-white">Sign In</Text>
         </Pressable>
       </Link>
       <Link href="/(auth)/sign-up" asChild>
-        <Pressable className="mt-4 rounded bg-primary px-6 py-3">
-          <Text className="text-center text-base text-white">Sign Up</Text>
-        </Pressable>
-      </Link>
-
-      <Link href="/subscriptions/spotify" asChild>
-        <Pressable className="mt-4 rounded bg-primary px-6 py-3">
-          <Text className="text-center text-base text-white">
-            Spotify Subscriptions
-          </Text>
-        </Pressable>
-      </Link>
-      <Link
-        href={{
-          pathname: "/subscriptions/[id]",
-          params: { id: "spotify" },
-        }}
-        asChild>
-        <Pressable className="mt-4 rounded bg-primary px-6 py-3">
+        <Pressable className="mt-4 font-sans-bold rounded bg-primary px-6 py-3">
           <Text className="text-center text-base text-white">Sign Up</Text>
         </Pressable>
       </Link>
